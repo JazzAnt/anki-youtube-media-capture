42dbbb6086d39fdbf5a8bd35b628157d60078e0f
Jason A. Januar <jason.kwee98@gmail.com>
2026-01-02 03:01:12 -0800

    add save button to settings (non-functional for now)

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,1 +1,12 @@
 console.log("Settings JS works!")
+
+var saveButton = document.getElementById("save-button");
+if (saveButton.addEventListener)
+    saveButton.addEventListener("click", onSaveButtonClick, false);
+else if (saveButton.attachEvent)
+    saveButton.attachEvent('onclick', onSaveButtonClick);
+
+
+function onSaveButtonClick() {
+    document.getElementById("button-message").innerHTML = "You Clicked The Button!"
+}
