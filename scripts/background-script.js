8b4eb18812caa6bf3d360f055466dafca84ab5ec
Jason A. Januar <jason.kwee98@gmail.com>
2026-01-06 04:42:54 -0800

    add function to validate anki connection status and toggle element visibility based on results
    now setting elements is only visible if connected to anki. If not, then the only element visible is the retry connection button

--- scripts/background-script.js
+++ scripts/background-script.js
@@ -3,9 +3,15 @@ import "./browser-polyfill.js";
 browser.runtime.onMessage.addListener(handleMessages);
 
 const Actions = {
-  "TEST": async (payload) => {
-    console.log(payload.message)
-    return { response: "Test Message from Background Service" }
+  "TEST-CONNECTION": async (payload) => {
+    try {
+    const result = await ankiConnectInvoke('version', 6);
+    console.log(`Got AnkiConnect Version: ${result}`);
+    return { response: true }
+    } catch (e) {
+    console.log(`Test Connection Failed: ${e}`);
+    return { response: false }
+    }
   }
 }
 
@@ -17,3 +23,27 @@ function handleMessages(message, sender) {
   return Promise.resolve({ error: `Action ${message.action} not found` })
 }
 
+async function ankiConnectInvoke(action, version, params={}) {
+  try
+  {
+    const response = await fetch('http://127.0.0.1:8765', {
+      method: 'POST',
+      body: JSON.stringify({action, version, params})
+    });
+
+    if(!response.ok){
+      throw new Error("Failed to connect to AnkiConnect")
+    }
+
+    const json = await response.json();
+
+    if(json.error){
+      throw json.error
+    }
+
+    return json.result;
+  }
+  catch (error) {
+    throw new Error(`AnkiConnect Error: ${error}`)
+  }
+}
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,13 +1,64 @@
+toggleSettingsVisibility(false)
+validateAnkiConnect()
+
+var connectButton = document.getElementById("connect-button");
+if (connectButton.addEventListener)
+  connectButton.addEventListener("click", validateAnkiConnect, false);
+else if (connectButton.attachEvent)
+  connectButton.addEventListener("onclick", validateAnkiConnect);
+
 var saveButton = document.getElementById("save-button");
 if (saveButton.addEventListener)
   saveButton.addEventListener("click", onSaveButtonClick, false);
 else if (saveButton.attachEvent)
   saveButton.attachEvent('onclick', onSaveButtonClick);
 
+async function validateAnkiConnect() {
+  const statusMessage = document.getElementById("status-message")
+  const connectButton = document.getElementById("connect-button")
+
+  connectButton.style.display= "none"
+  statusMessage.innerHTML = "Checking Connection..."
+  statusMessage.style.color = "blue"
+  const response = await callBackgroundService('TEST-CONNECTION', { message: "Hello from Popup!" })
+  connectButton.style.display= "inline-block"
+
+  //response.response here returns true if connection is successful
+  if (response.response) {
+    statusMessage.innerHTML = "Connected to Anki"
+    statusMessage.style.color = "green"
+    toggleSettingsVisibility(true)
+  }
+  else {
+    statusMessage.innerHTML = "Not Connected to Anki"
+    statusMessage.style.color = "red"
+    toggleSettingsVisibility(false)
+  }
+}
+
+function toggleSettingsVisibility(isVisible) {
+  const selectors = document.getElementsByClassName("selector-container")
+  const buttons = document.getElementsByClassName("button-container")
+
+  if (isVisible){
+    for(i = 0; i < selectors.length; i++){
+      selectors[i].style.display = "flex"
+    }
+    for(i = 0; i < buttons.length; i++){
+      buttons[i].style.display = "block"
+    }
+  } else {
+    for(i = 0; i < selectors.length; i++){
+      selectors[i].style.display = "none"
+    }
+    for(i = 0; i < buttons.length; i++){
+      buttons[i].style.display = "none"
+    }
+  }
+}
 
 async function onSaveButtonClick() {
-  const response = await callBackgroundService('TEST', {message: "Hello from Popup!"})
-  document.getElementById("button-message").innerHTML = "Response from background: " + response.response
+  document.getElementById("button-message").innerHTML = "You Clicked the Button"
 }
 
 async function callBackgroundService(action, payload = {}) {
